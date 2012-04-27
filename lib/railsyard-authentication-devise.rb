812a1e390ec6ecd96decd53402a39af2607fc53e
nebirhos <nebirhos@aol.com>
2012-04-27 02:54:29 +0200

    Rails routes helper

--- lib/railsyard-authentication-devise.rb
+++ lib/railsyard-authentication-devise.rb
@@ -1,5 +1,6 @@
 require 'devise'
 
 require 'railsyard-backend-authentication/devise' # FIXME: find better namespace
+require 'railsyard-backend-authentication/routes'
 require 'railsyard-authentication-devise/version'
 require 'railsyard-authentication-devise/engine'
--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,4 +1,4 @@
 Dummy::Application.routes.draw do
   root :to => "static#index"
-  devise_for :users, :controllers => { :sessions => "railsyard_authentication_devise/sessions" }
+  railsyard_authentication_devise_for :users
 end
