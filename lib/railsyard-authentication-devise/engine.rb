36431314f6ce3c722ee2153ad2fb0f40ae17a528
Stefano Verna <stefano.verna@welaika.com>
2012-05-04 12:18:44 +0200

    Stylized authentication box

--- lib/railsyard-authentication-devise/engine.rb
+++ lib/railsyard-authentication-devise/engine.rb
@@ -3,7 +3,7 @@ module RailsyardAuthenticationDevise
     config.to_prepare do
       Railsyard::Backend.plugin_manager.add_plugin(:authentication_devise) do
         name "Railsyard Devise Authentication"
-        backend_stylesheet_dependency "railsyard_authentication_devise/sidebar"
+        backend_stylesheet_dependency "railsyard_authentication_devise/authentication_box"
         authentication_infos "railsyard_authentication_devise/authentication_box"
       end
     end
